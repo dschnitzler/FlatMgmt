16003b641a836eb1afa8a358bf7ad2aec1c1667c
Daniel Schnitzler <Daniel.Schnitzler@alumni.fh-aachen.de>
2013-06-30 20:31:40 +0200

    kleine Aenderungen

--- index_money.php
+++ index_money.php
@@ -8,6 +8,15 @@ echo WriteHeader("&Uuml;bersicht", "money");
 echo "
 	<h2>Finanzen</h2>
 	<img src=\"Diagramm_money.php\"   width=\"770\" height=\"300\">
+	<div class=\"post-it\">
+	<h1>Monatliche Kosten</h1>
+		<ul>
+			* Miete        640&euro;<br>
+    		* Internet     20&euro;<br>
+    		* Strom/Gas    70&euro;<br>
+    		* Lebensmittel 200&euro;<br>
+    	</ul>
+	</div>
 	 ";
 echo WriteFooter();
 ?>
